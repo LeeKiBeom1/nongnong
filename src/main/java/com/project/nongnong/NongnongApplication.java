6c1c6e24100a2eaf4dcca5abb6845c8d0058d170
LeeKiBeom1 <soul3233@naver.com>
2022-11-21 11:17:07 +0900

    Initial commit

---
+++ src/main/java/com/project/nongnong/NongnongApplication.java
@@ -0,0 +1,13 @@
+package com.project.nongnong;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class NongnongApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(NongnongApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/project/nongnong/NongnongApplicationTests.java
@@ -0,0 +1,13 @@
+package com.project.nongnong;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class NongnongApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
