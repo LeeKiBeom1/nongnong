6fffcde4822db72be9d25e26d3325c6f092dfffd
LeeKiBeom1 <soul3233@naver.com>
2022-12-01 15:26:14 +0900

    commit_12_01

--- src/main/java/com/project/nongnong/service/BoardService.java
+++ src/main/java/com/project/nongnong/service/BoardService.java
@@ -1,16 +1,52 @@
 package com.project.nongnong.service;
 
+
+import com.project.nongnong.domain.BoardEntity;
+import com.project.nongnong.domain.UserEntity;
 import com.project.nongnong.dto.BoardDTO;
+import com.project.nongnong.dto.BoardPageResponseDTO;
+import com.project.nongnong.repository.BoardRepository;
+import com.project.nongnong.repository.UserRepository;
+import lombok.RequiredArgsConstructor;
+import lombok.extern.log4j.Log4j2;
+import org.springframework.stereotype.Service;
 
-public interface BoardService {
-    Long register(BoardDTO boardDTO);
+import javax.transaction.Transactional;
+import java.util.List;
 
-    BoardDTO readOnly(Long board_key);
+@RequiredArgsConstructor
+@Service
+@Log4j2
+public class BoardService {
 
-    void modify(BoardDTO boardDTO);
+    private final BoardRepository boardRepository;
+    private final UserRepository userRepository;
 
-    void remove(Long board_key);
+    @Transactional
+    public Long save(BoardDTO boardDTO) {
 
+        BoardEntity boardEntity = boardDTO.toEntity();
+
+        UserEntity userEntity = userRepository.findByUserkey(boardDTO.getUserkey());
+        log.info(userEntity.getUseremail());
+
+        boardEntity.setUserEntity(userEntity);
+
+        boardRepository.save(boardEntity);
+
+        return boardEntity.getBoardkey();
+
+    }
+
+    @Transactional
+    public List<BoardPageResponseDTO> list() {
+
+        List<BoardPageResponseDTO> list = null;
+
+
+
+        return list;
+    }
 
 
 }
--- src/main/java/com/project/nongnong/service/UserService.java
+++ src/main/java/com/project/nongnong/service/UserService.java
@@ -1,16 +1,45 @@
 package com.project.nongnong.service;
 
 
-import org.springframework.ui.Model;
+import com.project.nongnong.domain.UserEntity;
+import com.project.nongnong.dto.UserDTO;
+import com.project.nongnong.repository.UserRepository;
+import lombok.RequiredArgsConstructor;
+import lombok.extern.log4j.Log4j2;
+import org.springframework.stereotype.Service;
 
-// UserService 인터페이스와 Impl 을 따로 두고 사용하는 이유?
-public interface UserService {
+import javax.transaction.Transactional;
 
-// Impl에서 사용할 메소드를 인터페이스 형태로 만들어 놓는곳
-    // 추상 메소드 이기때문에 추상적인 명명의 메소드를 적고 실제적인 디비와 연동후 데이터를 받아오는 동작을 하는 객체는 UserServiceImpl이 담당 하고 있다.
-// 구현하고자 하는 부분이 명확하면서 제한적으로 내용이 드러나는 장점이 있다.
+@RequiredArgsConstructor
+@Service
+@Log4j2
+public class UserService {
 
-    // 회원 등록 하기
-    public void joinUser(Model model);
+    private final UserRepository userRepository;
+
+    @Transactional
+    public Long save(UserDTO userDTO) {
+        // DTO의 엔티티 변환메소드를 통해 엔티티클래스에 저장
+        UserEntity userEntity = userDTO.toEntity();
+        // 레포지토리를 이용해 디비와 연결해 저장
+        userRepository.save(userEntity);
+
+        // log
+        log.info(userEntity.getUserkey());
+
+        // 유저의 키값을 리턴해 프론트가 사용할수 있게 만듬
+        return userEntity.getUserkey();
+
+    }
+
+    @Transactional
+    public Long login(UserDTO userDTO) {
+        Long result = null;
+
+
+
+
+        return result;
+    }
 
 }
